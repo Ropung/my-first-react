599ae38f61014e2abe7efda2340396159b2d354f
Gihun <563710@naver.com>
2022-05-30 21:51:19 +0900

    useState를 사용해서 나이 변경

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,31 @@
-import { useState } from "react";
 import logo from "./logo.svg";
 import "./App.css";
+import { useState } from "react";
 
 function App() {
+  let justAge = 20;
+  const [age, setAge] = useState(20);
+
   return (
     <div className="App">
       <MainHeaderExample />
       <MainHeaderExample />
       <MainHeaderExample />
       <main>
-        <span>React Project</span>
+        <div>
+          <span>Just Age: {justAge}</span>
+          <br />
+          <span>Age using useState: {age}</span>
+          <button
+            onClick={function () {
+              justAge++;
+              const newAge = age + 1;
+              setAge(newAge); // 스토커 역할. age가 쓰인 모든 곳을 찾아가서 바꿔 줌.
+            }}
+          >
+            나이 증가
+          </button>
+        </div>
       </main>
     </div>
   );
